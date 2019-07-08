10bc312de250ed1225ad4ca3520a62eee848367d
S01TTN <adia3141@gmail.com>
2019-07-08 18:41:08 +0800

    完成Day1作業

---
+++ MVC_Homework/MVC_Homework/Controllers/AccountingController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_Homework.Models;
+
+namespace MVC_Homework.Controllers.ViewModels
+{
+    public class AccountingController : Controller
+    {
+        // GET: Accounting
+        public ActionResult Add()
+        {
+            return View();
+        }
+        [ChildActionOnly]
+        public ActionResult ShowAccounting()
+        {
+
+            List< AccountingNoteViewModel > accountingNote = new List<AccountingNoteViewModel>();
+            Random Rnd = new Random();
+            for (int i = 1; i < 100; i++)
+            {
+                
+                var result = new AccountingNoteViewModel()
+                {
+                    CostType = "支出",
+                    CostDate = DateTime.Now.AddDays(Rnd.Next(-100, 10)),
+                    CostMoney = Rnd.Next(1, 10000),
+                    Remark = "TEST"
+                };
+                accountingNote.Add(result);
+            }
+
+            return View(accountingNote);
+        }
+    }
+}
---
+++ MVC_Homework/MVC_Homework/Models/ViewModels/AccountingNoteViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Homework.Models
+{
+    public class AccountingNoteViewModel
+    {
+        [Display(Name = "類別")]
+        [Required(AllowEmptyStrings = false)]
+        public string CostType  { get; set; }
+        [Display(Name = "日期")]
+        public DateTime CostDate { get; set; }
+        [Display(Name = "金額")]
+        public int CostMoney { get; set; }
+        [Display(Name = "備註")]
+        public string Remark { get; set; }
+    }
+}
