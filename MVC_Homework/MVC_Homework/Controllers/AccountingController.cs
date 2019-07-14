bb4a73ba627320f8a79430e5eb141562314b01ce
S01TTN <adia3141@gmail.com>
2019-07-14 23:59:35 +0800

    將記帳本資料改成真實DB,使用EF

--- MVC_Homework/MVC_Homework/Controllers/AccountingController.cs
+++ MVC_Homework/MVC_Homework/Controllers/AccountingController.cs
@@ -9,26 +9,51 @@ namespace MVC_Homework.Controllers.ViewModels
 {
     public class AccountingController : Controller
     {
+        private Model1 db = new Model1();
         // GET: Accounting
         public ActionResult Add()
         {
             return View();
         }
+
+        // POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Add(AccountingNoteViewModel source)
+        {
+            if (ModelState.IsValid)
+            {
+                var distin = new AccountBook()
+                {
+                    Id = Guid.NewGuid(),
+                    Categoryyy = int.Parse(source.CostType),
+                    Amounttt = source.CostMoney,
+                    Dateee = source.CostDate,
+                    Remarkkk = source.Remark
+                };
+
+                db.AccountBook.Add(distin);
+                db.SaveChanges();
+            }
+            return View();
+        }
+
+
         [ChildActionOnly]
         public ActionResult ShowAccounting()
         {
 
-            List< AccountingNoteViewModel > accountingNote = new List<AccountingNoteViewModel>();
-            Random Rnd = new Random();
-            for (int i = 1; i < 100; i++)
+            List<AccountingNoteViewModel> accountingNote = new List<AccountingNoteViewModel>();
+
+            var fromDb = db.AccountBook.ToList();
+            foreach (var item in fromDb)
             {
-                
                 var result = new AccountingNoteViewModel()
                 {
-                    CostType = "支出",
-                    CostDate = DateTime.Now.AddDays(Rnd.Next(-100, 10)),
-                    CostMoney = Rnd.Next(1, 10000),
-                    Remark = "TEST"
+                    CostType = item.Categoryyy == CostTypeEnum.支出.GetHashCode() ? CostTypeEnum.支出.ToString() : CostTypeEnum.收入.ToString(),
+                    CostDate = item.Dateee,
+                    CostMoney = item.Amounttt,
+                    Remark = item.Remarkkk
                 };
                 accountingNote.Add(result);
             }
---
+++ MVC_Homework/MVC_Homework/Models/CostTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Homework.Models
+{
+    public enum CostTypeEnum
+    {
+        支出,
+        收入
+    }
+}
